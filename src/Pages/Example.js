b2742befbd3eab739b83f0ebe3fcaef967150fd4
yasushi <y1951511079@yahoo.co.jp>
2022-11-16 18:04:54 +0900

    Reactn121

--- src/Pages/Example.js
+++ src/Pages/Example.js
@@ -1,64 +1,62 @@
-/* eslint-disable */
-import {  useReducer } from "react";
+import { useState } from "react";
 
-const Example = () => {
+const Reminder = () => {
 
+    // const initialState = [{
+    //     id: 0,
+    //     content: ""
+    // }]
+    //stateは配列！オブジェクトに設定すると([]を付けないと)後にmapメソッドを使えない。
+    const [todos, setTodos] = useState([]);
+    //todoリストを保持
+    const [enteredTodo, setEnteredTodo] = useState("");
+    //Formの入力欄の状態を監視するstate。enteredTodoにはフォームに入力し文字が入ってくる。
 
+    const addList = (e) => {
 
-   const initialState = {
-        a: 0,
-        b: 0,
-        result: 0
-    }
-
-    const [state, dispatch] = useReducer((state, { type,payload }) => {
-  
-        switch (type) {
-            case "change":
-                const { name, value } = payload;
-                return { ...state, [name]: value };
-            case "plus":
-                return {...state, result:state.a + state.b};
-            case "minus":
-                return {...state, result:state.a - state.b};
-            case "multiply":
-                return{...state, result:state.a * state.b};
-            case "divide":
-                return {...state, result:state.a / state.b};
+        e.preventDefault();
+        //formのonClickのデフォルト動作である再リロードを防ぐ。
+        const newTodo = {
+            id: Math.floor(Math.random() * 1e5),
+            content: enteredTodo
         }
-    }, initialState)
-
-
-    const calcFn = (e) => {
-        dispatch({ type:e.target.value })
+        //enteredTodoの入力を元に新しいオブジェクトを生成し、これをtodoリストのstateに反映させる。
+        setTodos([...todos, newTodo])
+        setEnteredTodo("")
     }
-    const CALC_OPITONS = ["plus", "minus", "multiply", "divide"];
-    
-    const onChangeHandler = (e) =>{
-        dispatch({type: 'change', payload: {name: e.target.name, value: parseInt(e.target.value)}});
-      };
-    
+
+    const deleteTodo = (id) => {
+        const newTodos = todos.filter((todo) => {
+            return todo.id !== id;
+        });
+        setTodos(newTodos);
+    }
 
     return (
-        <div style={{ margin: 20 }}>
+        <>
+            <h1>Reminder</h1>
             <div>
-                <label style={{ marginRight: 10 }} htmlFor="id">a</label>
-                <input name="a" type="number" id="id" value={state.a} onChange={onChangeHandler} />
+                <h2>List.js</h2>
+                {todos.map(todo => {
+                    return (
+                        //todoには新規で登録されたオブジェクト{content:"××",id:○○}
+                        <div key={todo.id}>
+                            <button onClick={() => deleteTodo(todo.id)}>完了</button>
+                            <span>{todo.content}</span>
+                        </div>
+                    )
+                })}
             </div>
             <div>
-                <label style={{ marginRight: 10 }} htmlFor="id2">b</label>
-                <input name="b" type="number" id="id2" value={state.b} onChange={onChangeHandler} />
+                <h2>Form.js</h2>
+                <form onSubmit={addList}>
+                    <input type="text" value={enteredTodo} onChange={(e) => setEnteredTodo(e.target.value)} />
+                    <button  >追加</button>
+                </form>
             </div>
-            <select value={state.type} onChange={calcFn}>
-                {CALC_OPITONS.map((type)=>(
-                    <option key={type} value={type}>{type}</option>
-                ))}
-            </select>
-            <h2>{`結果：${state.result}`}</h2>
-
-        </div>
+        </>
     )
 
 }
 
-export default Example;
+export default Reminder;
